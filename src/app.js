aabc73f0f0ccb4a6f578ffcb747bf0962e0a4742
Claude Ishimwe <dpqb12haikuo@gmail.com>
2023-04-03 14:56:42 +0200

    Signup

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 import express from "express";
 import dbConnect from "./database/db";
-
+import userRouter from "./routes/userRoute"
 const app = express();
 dbConnect()
 
 app.use(express.json());
+app.use('/villageAPI/users', userRouter)
 
 export default app;
---
+++ src/helpers/bcrypt.js
@@ -0,0 +1,13 @@
+import bcrypt from "bcrypt";
+
+async function hashedPassword(password){
+    const salt = 12;  // it will take 12 rounds of salting to decrypt this password
+    const hashPassword = await bcrypt.hash(password, salt);
+    return hashPassword;
+}
+
+async function compareHashedPassword(candidatePassword, userPassword){
+    const isCorrect = await bcrypt.compare(candidatePassword, userPassword);
+    return isCorrect;
+}
+export default hashedPassword
